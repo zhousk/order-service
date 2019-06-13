8a96ae5d34f131171cc5ed3d3ad5aa817b254209
zhousk <sky.zhou@oocl.com>
2019-06-13 14:58:53 +0800

    eureka

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
